8505b5a0aac58cb475da4c3f3e49d4755ce0f9bb
Future <futureworldzone@gmail.com>
2023-07-23 12:50:09 +0300

    Completed the Lab for the test cases in README.md.

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+    ("Mr. Chocolate", 10, "M", "brown", "calm", 1),
+    ("Rowdy", 9, "M", "black", "anxious", 1),
+    ("Tabitha", 6, "F", "white", "lively", 1),
+    ("Sergeant Brown", 5, "M", "brown", "anxious", 1),
+    ("Melissa", 3, "F", "white", "calm", 1),
+    ("Grinch", 7, "M", "black", "aggressive", 0),
+    ("Wendy", 8, "F", "white", "lively", 1),
+    (null, 4, "F", "brown", "aggressive", 0); 
